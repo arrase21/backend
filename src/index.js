297637d223da10acacc3ece6c752bf228cc7dfc9
arrase <arrase21@gmail.com>
2022-12-10 18:15:51 -0500

    rollback

--- src/index.js
+++ src/index.js
@@ -7,19 +7,11 @@ const port = 3005
 // Enlazar la conexion en la base de datos
 
 conectarDB()
-
-var corsOptions = {
-  origin: 'https://macro.onrender.com',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.get('/products/:id', cors(corsOptions), function (req, res) {
-  res.json({msg: 'This is CORS-enabled for only example.com.'})
-})
-
-app.listen(80, function () {
-  console.log('CORS-enabled web server listening on port 80')
-});
+app.use(
+	cors({
+    origin: 'https://macro.onrender.com',
+	})
+);
 
 app.use(express.json())
 app.use('/api/clientes', require('../routes/cliente'))
