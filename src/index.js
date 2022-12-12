c05abe4d4889b876137e56edec47bc148d196dd3
arrase <arrase21@gmail.com>
2022-12-12 11:56:20 -0500

    Change cors

--- src/index.js
+++ src/index.js
@@ -1,25 +1,27 @@
 const express = require('express')
 const conectarDB = require('../config/db')
-const cors = require('cors')
 const app = express()
 const port = 3005
+const cors = require('cors')
 
 // Enlazar la conexion en la base de datos
 
 conectarDB()
+
 app.use(
 	cors({
-    origin: ['https://macro.onrender.com']
+    origin: URL = 'https://macro.onrender.com'
 	})
 );
 
 app.use(express.json())
-app.use('/api/clientes', require('../routes/cliente'))
-app.use('/api/alimentos', require('../routes/alimento'))
+app.use(URL = '/api/clientes', require('../routes/cliente'))
+app.use(URL = '/api/alimentos', require('../routes/alimento'))
 
-app.use('/api/auth', require('../routes/auth'))
 
-app.use('/api/usuarios', require('../routes/usuarios'))
+app.use(URL = '/api/auth', require('../routes/auth'))
+
+app.use(URL = '/api/usuarios', require('../routes/usuarios'))
 
 app.get('/', (req, res) => {
 	res.send('Bienvenidos, servidor configurado')
